76973564f38fdd10e2ec85e45e4c48ceeb252922
lucas41 <delfini.lucas@hotmail.com>
2022-07-11 22:14:45 -0300

    second_commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to recarregar.
+          Edite <code>src/App.js</code> and save to recarregar.
         </p>
         <a
           className="App-link"
