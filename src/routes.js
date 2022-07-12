f2cd92862402e7ca5f0913234fcc15ed4a5af8cd
lucas41 <delfini.lucas@hotmail.com>
2022-07-12 20:27:08 -0300

    commit-12-07

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './pages/home';
+import Filme from './pages/filme';
+
+import Header from './components/Header';
+
+function RouteApp() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/filme/:id" element={<Filme />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouteApp;
